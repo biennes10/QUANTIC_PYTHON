8341f94ef5980ad2db27f217ccb7edf62566149f
biennes10 <adrien.senut@gmail.com>
2025-06-26 13:45:36 +0200

    fix_login_logout

--- slots_game/urls.py
+++ slots_game/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='/login'), name='logout'),
     path('', views.welcome_view, name='home'), # La page d'accueil principale
     path('play_slots/', views.slots_game_view, name='play_slots'), # Endpoint pour le jeu via AJAX
     path('play_blackjack/', views.blackjack_game_view, name='play_blackjack'),
